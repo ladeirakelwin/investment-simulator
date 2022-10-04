7f684b93d0273147103c4c53b9ddb2df361f9196
ladeirakelwin <ladeirakelwin@gmail.com>
2022-10-04 09:13:56 -0300

    Inicio da inserção do zustand

--- components/MyInput/index.tsx
+++ components/MyInput/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
+import { simulatorData } from '../../pages';
 
 // import { Container } from './styles';
 interface IMyInput {
@@ -8,14 +9,15 @@ interface IMyInput {
     classLabel?: string;
     type?: string;
     placeholder?: string;
-    
+	name: string;
+	
 }
 
-const MyInput: React.FC<IMyInput> = ({ controlId,label,classLabel,placeholder,type='text'}) => {
+const MyInput: React.FC<IMyInput> = ({ controlId,label,classLabel,placeholder, name,type='text'}) => {
 	return (
 		<Form.Group className='mb-3' controlId={controlId}>
 			<Form.Label className={classLabel}>{label}</Form.Label>
-			<Form.Control type={type} placeholder={placeholder} className="rounded-0 bg-transparent border border-dark border-top-0 border-end-0 border-start-0" />
+			{/* <Form.Control type={type} placeholder={placeholder} className="rounded-0 bg-transparent border border-dark border-top-0 border-end-0 border-start-0" value={value} onChange={(event) => changer((prev) => {...prev, state: event.target.value})}/> */}
 		</Form.Group>
 	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,54 @@ import styles from '../styles/Home.module.scss';
 import NameChoice from '../components/NameChoice';
 import MyInput from '../components/MyInput';
 import MyButtonGroup from '../components/MyButtonGroup';
+import { useEffect, useState } from 'react';
+
+export type simulatorData = {
+	initial: number | undefined;
+	due: number | undefined;
+	ipca: number | undefined;
+	monthly: number | undefined;
+	profitability: number | undefined;
+	cdi: number | undefined;
+};
+
+interface IIPCAandCDI {
+	nome: string;
+	valor: number;
+}
+
+type InfoIndicator = {
+	[key: string]: number;
+};
+
+function getIPCAandCDI(currentState: simulatorData, callback: Function) {
+	return fetch('http://localhost:3000/indicadores')
+		.then((response) => response.json())
+		.then((data: IIPCAandCDI[]) => {
+			const newData: InfoIndicator = {} as InfoIndicator;
+			data.forEach(({ nome, valor }) => {
+				newData[nome] = valor;
+			});
+			callback({ ...currentState, ...newData });
+		});
+}
 
 const Home: NextPage = () => {
+	const [formData, setFormData] = useState<simulatorData>({
+		initial: undefined,
+		due: undefined,
+		ipca: undefined,
+		monthly: undefined,
+		profitability: undefined,
+		cdi: undefined,
+	});
+
+	console.log(formData);
+
+	useEffect(() => {
+		getIPCAandCDI(formData, setFormData);
+	}, []);
+
 	return (
 		<div className={styles.container}>
 			<Container fluid className=" p-4">
@@ -16,21 +62,28 @@ const Home: NextPage = () => {
 					<div>
 						<h4>Simulador</h4>
 						<Form>
-							<Stack direction='horizontal' className='d-flex justify-content-center justify-content-sm-evenly justify-content-lg-between mb-4 flex-wrap'>
+							<Stack
+								direction="horizontal"
+								className="d-flex justify-content-center justify-content-sm-evenly justify-content-lg-between mb-4 flex-wrap"
+							>
 								<div>
 									<NameChoice name="Rendimento" />
-                  <MyButtonGroup names={['Bruto','Líquido']}/>
-									<MyInput label="Aporte Inicial" />
-									<MyInput label="Prazo (em meses)" type="number" />
-									<MyInput label="IPCA (ao ano)" type="number"/>
+									<MyButtonGroup names={['Bruto', 'Líquido']} />
+									<MyInput label="Aporte Inicial" type="number" name="initial" />
+									<MyInput label="Prazo (em meses)" type="number" name="due" />
+									<MyInput label="IPCA (ao ano)" type="number" name="ipca" />
 								</div>
 
 								<div>
 									<NameChoice name="Tipo de indexação" />
-                  <MyButtonGroup names={['Pré','Pós','Fixado']}/>
-									<MyInput label="Aporte Mensal" />
-									<MyInput label="Rentabilidade" type="number" />
-									<MyInput label="CDI (ao ano)" type="number" />
+									<MyButtonGroup names={['Pré', 'Pós', 'Fixado']} />
+									<MyInput label="Aporte Mensal" type="number" name="monthly" />
+									<MyInput
+										label="Rentabilidade"
+										type="number"
+										name="profitability"
+									/>
+									<MyInput label="CDI (ao ano)" type="number" name="cdi" />
 								</div>
 							</Stack>
 							<div className="d-flex flex-wrap flex-column flex-lg-row justify-content-lg-between align-items-center">
