0bb0dd2bf1cf743c21a9aa5fcd939ea0b4291b75
ladeirakelwin <ladeirakelwin@gmail.com>
2022-10-07 09:24:19 -0300

    feat: base dos inputs numéricos concluída

--- components/MyInput/index.tsx
+++ components/MyInput/index.tsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
-import { simulatorData } from '../../pages';
+import { NumericFormat } from 'react-number-format';
 
 // import { Container } from './styles';
 interface IMyInput {
-    controlId?: string;
-    label: string;
-    classLabel?: string;
-    type?: string;
-    placeholder?: string;
+	label: string;
+	classLabel?: string;
+	placeholder?: string;
 	name: string;
-	
+	value?: string | number;
+	onChange: (e: Partial<SimulatorData>) => void;
+	prefix?: string;
+	suffix?: string;
 }
 
-const MyInput: React.FC<IMyInput> = ({ controlId,label,classLabel,placeholder, name,type='text'}) => {
+type SimulatorData = {
+	initial: string | undefined;
+	due: string | undefined;
+	ipca: string | undefined;
+	monthly: string | undefined;
+	profitability: string | undefined;
+	cdi: string | undefined;
+};
+
+const MyInput: React.FC<IMyInput> = ({
+	label,
+	classLabel,
+	placeholder,
+	name,
+	value,
+	onChange,
+	prefix,
+	suffix,
+}) => {
 	return (
-		<Form.Group className='mb-3' controlId={controlId}>
+		<Form.Group className="mb-3">
 			<Form.Label className={classLabel}>{label}</Form.Label>
-			{/* <Form.Control type={type} placeholder={placeholder} className="rounded-0 bg-transparent border border-dark border-top-0 border-end-0 border-start-0" value={value} onChange={(event) => changer((prev) => {...prev, state: event.target.value})}/> */}
+			<NumericFormat prefix={prefix} suffix={suffix} decimalScale={2} decimalSeparator="," onChange={(e) => onChange({[name]: e.target.value})} value={value} customInput={Form.Control} className="rounded-0 bg-transparent border border-dark border-top-0 border-end-0 border-start-0" />
 		</Form.Group>
 	);
 };
