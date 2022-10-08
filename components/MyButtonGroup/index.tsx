d971d6a847897835100e313efeafeea6f2da6e44
ladeirakelwin <ladeirakelwin@gmail.com>
2022-10-07 23:16:15 -0300

    feat: mudando a classe atived para o botão clicado

--- components/MyButtonGroup/index.tsx
+++ components/MyButtonGroup/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import styles from './MyButtonGroup.module.scss'
+import { type } from 'os';
+import React, { ReactElement, SyntheticEvent } from 'react';
+import { SyntheticInputEvent } from 'react-number-format/types/types';
+import styles from './MyButtonGroup.module.scss';
 // import { Container } from './styles';
 // Cada nome passado seja um botão
 // O botão selecionado seja orange
@@ -8,22 +10,44 @@ interface IMyButtonGroup {
 	names: string[];
 }
 
+const setActive = (el: SyntheticEvent<HTMLButtonElement>) => {
+	const sibblingElements = el?.currentTarget?.parentElement?.children;
+	if (sibblingElements) {
+		const length = sibblingElements.length;
+		for (let i = 0; i < length; i++) {
+			sibblingElements[i].classList.remove(styles.actived);
+		}
+		el.currentTarget.classList.add(styles.actived);
+	}
+};
+
 const MyButtonGroup: React.FC<IMyButtonGroup> = ({ names }) => {
 	const first = 0;
 	const last = names.length - 1;
+
 	return (
 		<div className={styles['button-container']}>
 			{names.map((name, index) => {
 				return index == first ? (
-					<button key={index} type="button" className={`rounded-start ${styles.actived}`}>
+					<button
+						key={index}
+						type="button"
+						className={`rounded-start ${styles.actived}`}
+						onClick={(e) => setActive(e)}
+					>
 						{name}
 					</button>
 				) : index == last ? (
-					<button key={index} type="button" className="rounded-end">
+					<button
+						key={index}
+						type="button"
+						className="rounded-end"
+						onClick={(e) => setActive(e)}
+					>
 						{name}
 					</button>
 				) : (
-					<button key={index} type="button" className="">
+					<button key={index} type="button" className="" onClick={(e) => setActive(e)}>
 						{name}
 					</button>
 				);
