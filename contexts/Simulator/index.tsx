b08d40655be9646e479bd1c1232551a8f04f77ad
ladeirakelwin <ladeirakelwin@gmail.com>
2022-10-10 14:37:38 -0300

    feat: adicionando 2 campos para index e rendimento no contexto

--- contexts/Simulator/index.tsx
+++ contexts/Simulator/index.tsx
@@ -1,17 +1,15 @@
 import create from 'zustand';
-type Data = {
-	initial: string | undefined;
-	due: string | undefined;
-	ipca: string | undefined;
-	monthly: string | undefined;
-	profitability: string | undefined;
-	cdi: string | undefined;
-};
-type SimulatorData = {
-	data: Data;
-	setALot: (data: Partial<Data>) => void;
-	cleanState: () => void;
-};
+
+enum IndexingType {
+	PRE = 'Pré',
+	POS = 'Pós',
+	FIXADO = 'Fixado',
+}
+
+enum Performance {
+	BRUTO = 'Bruto',
+	LIQUIDO = 'Líquido',
+}
 
 const initialState = {
 	data: {
@@ -21,8 +19,28 @@ const initialState = {
 		monthly: '',
 		profitability: '',
 		cdi: '',
+		performance: Performance.BRUTO,
+		indexing: IndexingType.PRE,
 	},
 };
+
+type Data = {
+	initial: string | undefined;
+	due: string | undefined;
+	ipca: string | undefined;
+	monthly: string | undefined;
+	profitability: string | undefined;
+	cdi: string | undefined;
+	performance: Performance;
+	indexing: IndexingType;
+};
+
+type SimulatorData = {
+	data: Data;
+	setALot: (data: Partial<Data>) => void;
+	cleanState: () => void;
+};
+
 export const useSimulatorStore = create<SimulatorData>((set) => ({
 	...initialState,
 	setALot: (newData: Partial<Data>) =>
