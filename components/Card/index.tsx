2f8764c11017e0e4ef3f0232dd5e4a214f44ac1e
ladeirakelwin <ladeirakelwin@gmail.com>
2022-10-18 11:24:21 -0300

    feat: base do card finalizada

---
+++ components/Card/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { CardText, CardTextKeys } from '../../shared/constants';
+
+interface CardProps {
+	title: CardTextKeys;
+	value: string | number;
+	className?: string;
+	isGreen?: boolean;
+	typeValue?: 'currency' | 'percentage';
+}
+
+const Card: React.FC<CardProps> = ({
+	title,
+	value,
+	className,
+	isGreen = false,
+	typeValue = 'currency',
+}) => {
+	const hasGreen = isGreen ? 'text-success' : 'text-dark';
+	const tittle = CardText[title] || title;
+	const formatValue =
+		typeValue === 'currency'
+			? new Intl.NumberFormat('pt-BR', {
+					style: 'currency',
+					currency: 'BRL',
+					minimumFractionDigits: 2,
+					maximumFractionDigits: 2,
+			  }).format(Number(value))
+			: new Intl.NumberFormat('pt-BR', {
+					style: 'percent',
+					minimumFractionDigits: 2,
+					maximumFractionDigits: 2,
+			  }).format(Number(value));
+	return (
+		<div
+			className={`shadow-sm bg-light d-flex flex-column
+		space-between flex-fill text-center gap-3 p-2 ${className}`}
+		>
+			<span className="fw-bold">{tittle}</span>
+			<span className={`${hasGreen} fs-6`}>{formatValue}</span>
+		</div>
+	);
+};
+
+export default Card;
