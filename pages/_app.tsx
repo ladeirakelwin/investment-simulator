c0549d53be10c02033fc7b5af7b7d930385d2fee
ladeirakelwin <ladeirakelwin@gmail.com>
2022-10-24 15:05:00 -0300

    feat: desativando RQDT do app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,18 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
-import '../node_modules/react-bootstrap/dist/react-bootstrap.min.js'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
+import '../node_modules/react-bootstrap/dist/react-bootstrap.min.js';
+import { QueryClient, QueryClientProvider } from 'react-query';
+// import { ReactQueryDevtools } from 'react-query/devtools';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <QueryClientProvider client={queryClient}>
-    <Component {...pageProps} />
-    <ReactQueryDevtools initialIsOpen={false} />
-
-  </QueryClientProvider>
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Component {...pageProps} />
+			{/* <ReactQueryDevtools initialIsOpen={false}  /> */}
+		</QueryClientProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
